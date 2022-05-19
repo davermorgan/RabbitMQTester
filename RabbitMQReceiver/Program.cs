b8249d5e4a25feb4006beabf47daadf6a8cb7085
David Morgan <David.Morgan@Civica.co.uk>
2022-05-19 09:39:25 +0100

    Added random failure to main queue consumer

--- RabbitMQReceiver/Program.cs
+++ RabbitMQReceiver/Program.cs
@@ -17,26 +17,26 @@ using (var channel = connection.CreateModel())
     {
         var body = ea.Body.ToArray();
         var message = Encoding.UTF8.GetString(body);
-       // Console.WriteLine("Received {0}", message);
+        // Console.WriteLine("Received {0}", message);
 
         //int dots = message.Split('.').Length - 1;
         //Thread.Sleep(dots * 1000);
 
-       // Console.WriteLine(" [x] Done {0}", message);
+        // Console.WriteLine(" [x] Done {0}", message);
 
-        //int randomNumber = new Random().Next(1, 3);
+        int randomNumber = new Random().Next(1, 3);
 
-        //if (randomNumber == 1)
-        //{
-        //    Console.WriteLine($"Ack {message}");
-        //    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
-        //}
-        //else
-        //{
+        if (randomNumber == 1)
+        {
+            Console.WriteLine($"Ack {message}");
+            channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+        }
+        else
+        {
             // Fail the message, some transient issue occurred - this would be triggered by an exception in real life but we just do it randomly here!
             Console.WriteLine($"Failed {message}");
             channel.BasicReject(deliveryTag: ea.DeliveryTag , requeue: false);
-        //}        
+        }        
     };
     channel.BasicConsume(queue: "mainQueue", autoAck: false, consumer: consumer);
 
